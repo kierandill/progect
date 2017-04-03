17d000a1236a7a38d47a0fc6d865ca45f9500cfe
KFD dank memes for life <kieran.dill18@alumni.loyola.ca>
2017-04-03 13:49:41 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -1,3 +1,21 @@
-function meme(){
-new Date(2017, 01, 12, 11, 18, )
-}
+ function login() { /* set up variable person*/
+	var person = prompt("what is your username");
+	var password = prompt("what is your password");
+	
+	if (person === "kieran"){
+		/* only happens if u get the user name*/
+		/* to make the password*/
+		
+		if (password === "dill"){
+			alert("welcome to the page");
+			location.href = "universal_page.html";
+	}else{
+			alert("invalid username");	
+		}
+	}else{
+		alert("invalid password"); 
+	 }								
+	/* if it is a wrong username*/
+	
+  
+ }
